53a648e063ff0229f9c093acd3daebe04aea0b6d
Ritika <ritika.sharma@sourcefuse.com>
2024-12-26 12:12:32 +0530

    fix: remove loopback example

--- services/loopback4-example-todo/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationConfig, TodoListApplication } from './application';
-export * from '@loopback/rest';
-export * from './application';
-export * from './models';
-export * from './repositories';
-export declare function main(options?: ApplicationConfig): Promise<TodoListApplication>;
