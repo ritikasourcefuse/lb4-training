58a04d623149cbc6062b62c741ddc279d975342a
Ritika <ritika.sharma@sourcefuse.com>
2024-12-31 10:24:25 +0530

    refactor: removed unwanted files and changes

--- packages/shared/src/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './jwt-service';
-export * from './key';
-export * from './types';
-export * from './auth-strategies/jwt-strategy';
--- packages/shared/src/auth/key.ts
+++
@@ -1,9 +0,0 @@
-export const TokenServiceBindings = {
-  SECRET: 'authentication.jwt.secret',
-  EXPIRES_IN: 'authentication.jwt.expiresIn',
-  SERVICE: 'services.authentication.jwt.tokenservice',
-};
-
-export const AuthorizationBindings = {
-  PROVIDER: 'authorization.role-based.provider',
-};
